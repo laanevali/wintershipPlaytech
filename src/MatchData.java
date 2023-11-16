20b1ee2b2883f5b34cc56bf4c0004c1a64f91c80
laanevali <95487054+laanevali@users.noreply.github.com>
2023-11-16 11:58:52 +0200

    Data classes
    Making player and match data classes

---
+++ src/MatchData.java
@@ -0,0 +1,22 @@
+public class MatchData {
+    private String id;
+    private double rateA;
+    private double rateB;
+    private String result;
+
+    public String getId() {
+        return id;
+    }
+
+    public double getRateA() {
+        return rateA;
+    }
+
+    public double getRateB() {
+        return rateB;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
---
+++ src/PlayerData.java
@@ -0,0 +1,27 @@
+public class PlayerData {
+    private String id;
+    private String transaction;
+    private String matchId;
+    private int coinAmount;
+    private String bet;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTransaction() {
+        return transaction;
+    }
+
+    public String getMatchId() {
+        return matchId;
+    }
+
+    public int getCoinAmount() {
+        return coinAmount;
+    }
+
+    public String getBet() {
+        return bet;
+    }
+}
